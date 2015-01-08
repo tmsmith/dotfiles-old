ae4fe72281beed1bb35787f600c381236aa8a3f8
AUTHORNAME <AUTHOREMAIL>
2015-01-08 10:16:31 -0500

    added boxen and updated alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,6 +12,8 @@ alias st='${_subl} -n'
 alias stt='st .'
 
 alias c='clear'
+alias cls='clear'
 alias cll='clear && ll'
 alias cla='clear && la'
 alias flushdns='sudo dscacheutil -flushcache' # OSX ipconfig /flushdns
+alias x='exit'
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,4 @@
 export GOPATH=$HOME/src/go
 export PATH=$PATH:/opt/boxen/homebrew/Cellar/go/1.3/libexec/bin:$GOPATH/bin
+
+source /opt/boxen/env.sh
